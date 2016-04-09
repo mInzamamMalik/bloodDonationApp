06205cc187a2f13fcf7e1758392ed078d697be8e
malikasinger1@gmail.com <malikasinger1@gmail.com>
2016-04-10 00:12:07 +0500

    hosted on firebase

---
+++ www/js/mainService.js
@@ -0,0 +1,10 @@
+/**
+ * Created by 205 on 4/9/2016.
+ */
+angular.module('starter')
+  .service("mainService",function(){
+
+    this.userRef = new Firebase("https://shareblood.firebaseio.com/users");
+
+
+  })
